73d0af762557ee5f46ed7b8749e680db3490bf21
Jorge Benjumea Burillo <me@intron014.com>
2022-12-09 18:06:51 +0100

    Catch up to P12

---
+++ [TP] - Practica 12/main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_LINE_LEN 1024
+
+
+int count_words(FILE *file) {
+    int word_count = 0;
+    char c;
+
+    while ((c = fgetc(file)) != EOF) {
+        if (c != ' ' && c != '\n') {
+            word_count++;
+
+            while ((c = fgetc(file)) != EOF && c != ' ' && c != '\n') {}
+        }
+    }
+
+    return word_count;
+}
+
+char largest_line(FILE *file) {
+    char line[MAX_LINE_LEN] = "";
+    int max_words = 0;
+    char longest_line[MAX_LINE_LEN] = "";
+
+    while (fgets(line, MAX_LINE_LEN, file)) {
+        int words = 0;
+        for (int i = 0; i <= strlen(line); i++) {
+            if (line[i] == ' ' || line[i] == '\n') {
+                words++;
+            }
+        }
+        if (words > max_words) {
+            max_words = words;
+            strcpy(longest_line, line);
+        }
+    }
+    printf("Line with the most words: %s\n", longest_line);
+}
+void remove_chars(FILE *infile, char *outfile_name, char *chars) {
+    FILE *outfile = fopen(outfile_name, "w");
+    char line[MAX_LINE_LEN];
+    while (fgets(line, MAX_LINE_LEN, infile)) {
+        char buffer[MAX_LINE_LEN];
+        int buffer_index = 0;
+        for (int i = 0; i < strlen(line); i++) {
+            if (strchr(chars, line[i]) == NULL) {
+                buffer[buffer_index] = line[i];
+                buffer_index++;
+            }
+        }
+
+        buffer[buffer_index] = '\n';
+        buffer_index++;
+
+        fwrite(buffer, sizeof(char), buffer_index, outfile);
+    }
+
+    fclose(outfile);
+}
+
+
+    int main(){
+    int word_count;
+    FILE *file = fopen("input.txt", "r");
+    if (!file) {
+        printf("Error: The file couldn't be opened.\n");
+        return 1;
+    }
+    word_count=count_words(file);
+    printf("The file contains %d words\n", word_count);
+    fclose(file);
+    FILE *file2 = fopen("input.txt", "r");
+    if (!file2) {
+        printf("Error: The file couldn't be opened.\n");
+        return 2;
+    }
+    largest_line(file2);
+    fclose(file2);
+    FILE *infile = fopen("input.txt", "r");
+    remove_chars(infile, "salida.txt", "abc");
+    fclose(infile);
+}
+
+
